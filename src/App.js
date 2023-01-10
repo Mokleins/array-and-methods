fdeb5bae53bf6bfa57af47b03e27a6d51b3db556
Xylon Funes <xyfullns@gmail.com>
2023-01-09 23:06:58 -0500

    commiting

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-
 function App() {
+  
+  const family = ['Mariia', 'Marina', 'Darina', 'Yenia'];
+  
+  console.log(family)
+
   return (
     <div className="App">
       <header className="App-header">
-   
+      
       </header>
     </div>
   );
