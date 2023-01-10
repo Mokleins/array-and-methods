0f323fff6e7f698514d6528da3a2108be7eeef89
Xylon Funes <xyfullns@gmail.com>
2023-01-09 23:26:06 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 function App() {
   
-  const family = ['Mariia', 'Marina', 'Darina', 'Yenia'];
+  const family = ['Mariia', 'Marina', 'Darina', 'Yenia', 'Xylon'];
   
-  console.log(family)
+  console.log(family);
 
   return (
     <div className="App">
       <header className="App-header">
-      
+      {family.map((x) => {
+        return <p>array = {x}</p>;
+      })}
       </header>
     </div>
   );
