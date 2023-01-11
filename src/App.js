de3858b2527fefd8523609f53a4fba00b2eca195
Xylon Funes <xyfullns@gmail.com>
2023-01-10 23:42:59 -0500

    commiting

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 function App() {
   
   const family = ['Mariia', 'Marina', 'Darina', 'Yenia', 'Xylon'];
-  
+  const familytwo = ['Xylon', 'Louis', 'Efrain', 'Monica'];
   console.log(family);
 
   return (
     <div className="App">
       <header className="App-header">
-      {family.map((x, i) => {
+      { 
+      family.map((x, i) => {
         return <p>index = {i}: {x} </p>;
-      })} 
+      })
+      } 
+      {<p>Another Family Array</p>}
+      {
+        familytwo.map((x, i) => {
+          return <p>index = {i}: {x}</p>
+        })
+      }
       </header>
     </div>
   );
