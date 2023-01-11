9f333ddd377bf87d71e54d6be20232b6c90d9190
Xylon Funes <xyfullns@gmail.com>
2023-01-11 17:56:22 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,38 +1,10 @@
 import UnshiftAndPush from "./components/UnshiftAndPush";
 
 function App() {
-  
-  const family = ['Mariia', 'Marina', 'Darina', 'Yenia', 'Xylon'];
-  const familytwo = ['Xylon', 'Louis', 'Efrain', 'Monica'];
-  family.push('Un nuevo elemento');
-  console.log(family);
-
   return (
-    <div className="App">
+    <div className="App" >
       <header className="App-header">
-      { 
-      family.map((x, i) => {
-        return <p>index = {i}: {x} </p>;
-      })
-      } 
-      {<p>Another Family Array</p>}
-      {
-        familytwo.map((x, i) => {
-          return <p>index = {i}: {x}</p>
-        })
-      }
-      {
-        <p>When the developer decides to use the method push, it will add elements to the end 
-          of the array. There is another method that is called unshift() which adds to the 
-          begging of an Array.
-          <p>for example if the user decided to use push element to the family array it will look like this</p> 
-          <p> Este nuevo elemento fue agreago con el array.</p>
-        </p>
-        
-      }
-      {
-
-      }
+      <UnshiftAndPush/>
       </header>
     </div>
   );
--- src/components/UnshiftAndPush.js
+++ src/components/UnshiftAndPush.js
@@ -1,8 +1,35 @@
 import {React, useState} from "react";
 
 function UnshiftAndPush() {
+    const family = ['Mariia', 'Marina', 'Darina', 'Yenia', 'Xylon'];
+    const familytwo = ['Xylon', 'Louis', 'Efrain', 'Monica'];
+    family.push('Un nuevo elemento');
+    console.log(family);
 
-    return (<p>hello</p>); 
+
+    return (<>
+    <button></button>
+    { 
+      family.map((x, i) => {
+        return <p>index = {i}: {x} </p>;
+      })
+      } 
+      {<p>Another Family Array</p>}
+      {
+        familytwo.map((x, i) => {
+          return <p>index = {i}: {x}</p>
+        })
+      }
+      {
+        <p>When the developer decides to use the method push, it will add elements to the end 
+          of the array. There is another method that is called unshift() which adds to the 
+          begging of an Array.
+          <p>for example if the user decided to use push element to the family array it will look like this</p> 
+          <p> Este nuevo elemento fue agreago con el array.</p>
+        </p>
+        
+      }
+    </>); 
 }
 
 export default UnshiftAndPush;
