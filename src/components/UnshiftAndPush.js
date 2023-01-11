0e62d9f2b4739e10aea622077f2bc5c43560a53d
Xylon Funes <xyfullns@gmail.com>
2023-01-11 18:33:22 -0500

    c

--- src/components/UnshiftAndPush.js
+++ src/components/UnshiftAndPush.js
@@ -1,22 +1,27 @@
 import { React, useState } from "react";
 
 function UnshiftAndPush() {
+  const [unshift, setUnshift] = useState("");
+  const [push, setPush] = useState("");
   const family = ["Mariia", "Marina", "Darina", "Yenia", "Xylon"];
   const familytwo = ["Xylon", "Louis", "Efrain", "Monica"];
   family.push("Un nuevo elemento");
   console.log(family);
+  
 
   return (
     <>
       <button>Unshift</button>
+      <input type="text" id="unshift" value={unshift} onChange={e => setUnshift(e.target.value)}></input>
       {family.map((x, i) => {
         return (
           <p>
-            index = {i}: {x}{" "}
+            index = {i}: {x}{""}
           </p>
         );
       })}
       <button>Push</button>
+      <input type="text" id="push" value={push} onChange={e => setPush(e.target.value)}></input>
       <p>Another Family Array</p>
       {familytwo.map((x, i) => {
         return (
