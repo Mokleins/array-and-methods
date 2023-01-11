d16572361bb9ed40e8e85270de296b843a802868
Xylon Funes <xyfullns@gmail.com>
2023-01-11 18:07:37 -0500

    m

--- src/components/UnshiftAndPush.js
+++ src/components/UnshiftAndPush.js
@@ -1,35 +1,43 @@
-import {React, useState} from "react";
+import { React, useState } from "react";
 
 function UnshiftAndPush() {
-    const family = ['Mariia', 'Marina', 'Darina', 'Yenia', 'Xylon'];
-    const familytwo = ['Xylon', 'Louis', 'Efrain', 'Monica'];
-    family.push('Un nuevo elemento');
-    console.log(family);
+  const family = ["Mariia", "Marina", "Darina", "Yenia", "Xylon"];
+  const familytwo = ["Xylon", "Louis", "Efrain", "Monica"];
+  family.push("Un nuevo elemento");
+  console.log(family);
 
+  return (
+    <>
+      <button>Unshift</button>
+      {family.map((x, i) => {
+        return (
+          <p>
+            index = {i}: {x}{" "}
+          </p>
+        );
+      })}
+      <button>Push</button>
+      <p>Another Family Array</p>
+      {familytwo.map((x, i) => {
+        return (
+          <p>
+            index = {i}: {x}
+          </p>
+        );
+      })}
+      <p>
+        When the developer decides to use the method push, it will add elements
+        to the end of the array. There is another method that is called
+        unshift() which adds to the begging of an Array.
+      </p>
+      <p>
+        for example if the user decided to use push element to the family array
+        it will look like this
+      </p>
 
-    return (<>
-    <button></button>
-    { 
-      family.map((x, i) => {
-        return <p>index = {i}: {x} </p>;
-      })
-      } 
-      {<p>Another Family Array</p>}
-      {
-        familytwo.map((x, i) => {
-          return <p>index = {i}: {x}</p>
-        })
-      }
-      {
-        <p>When the developer decides to use the method push, it will add elements to the end 
-          of the array. There is another method that is called unshift() which adds to the 
-          begging of an Array.
-          <p>for example if the user decided to use push element to the family array it will look like this</p> 
-          <p> Este nuevo elemento fue agreago con el array.</p>
-        </p>
-        
-      }
-    </>); 
+      <p> Este nuevo elemento fue agreago con el array</p>
+    </>
+  );
 }
 
 export default UnshiftAndPush;
