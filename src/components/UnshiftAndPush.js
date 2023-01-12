181ab34020d5dd67c129c58fd63bc5baa7a2d608
Xylon Funes <xyfullns@gmail.com>
2023-01-11 19:17:35 -0500

    commit

--- src/components/UnshiftAndPush.js
+++ src/components/UnshiftAndPush.js
@@ -3,15 +3,31 @@ import { React, useState } from "react";
 function UnshiftAndPush() {
   const [unshift, setUnshift] = useState("");
   const [push, setPush] = useState("");
-  const family = ["Mariia", "Marina", "Darina", "Yenia", "Xylon"];
+  const [family, setFamily] = useState([]);
   const familytwo = ["Xylon", "Louis", "Efrain", "Monica"];
-  family.push("Un nuevo elemento");
+  // family.push("Un nuevo elemento");
   console.log(family);
-  
+  console.log(unshift);
+
+  function fam() {
+
+      setFamily(["Mariia", "Marina", "Darina", "Yenia", "Xylon"]);
+  }
+  fam()
+
+  console.log(family);
+
+  function handleUnshiftSubmit(e) {
+    e.preventDefault();
+    console.log(unshift);
+    let new_family = family.unshift(unshift);
+    setFamily(new_family);
+  }
+
 
   return (
     <>
-      <button>Unshift</button>
+      <button onSubmit={handleUnshiftSubmit}>Unshift</button>
       <input type="text" id="unshift" value={unshift} onChange={e => setUnshift(e.target.value)}></input>
       {family.map((x, i) => {
         return (
