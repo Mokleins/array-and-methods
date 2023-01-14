ff79c67eddb8fba13e9a8bb4a0aa6dafa2f5713f
Xylon Funes <xyfullns@gmail.com>
2023-01-13 23:28:37 -0500

    slice method

--- src/components/Slice.js
+++ src/components/Slice.js
@@ -1,38 +1,61 @@
-import {React, useEffect, useState} from "react";
+import { React, useEffect, useState } from "react";
 
-function Slice () {
-    const [first, setFirst] = useState("");
-    const [second, setSecond] = useState("");
-    const [array, setArray] = useState([]);
+function Slice() {
+  const [first, setFirst] = useState("");
+  const [second, setSecond] = useState("");
+  const [array, setArray] = useState([]);
 
-    useEffect(() => {
-        return() => {
-            setArray(["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]);
-        };
-    }, []);
+  useEffect(() => {
+    return () => {
+      setArray(["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]);
+    };
+  }, []);
 
-    console.log(array)
+  console.log(array);
 
-    return (
-      <>
-        <div className="cards">
-          <div className="card">
-            <h3>.slice()</h3>
-            <p>it can take 2 arguments: the first is the index where the slice should begin and the second is the index before which it should end</p>
-            <div>
-                {array.map((x, i) => {
-                    return(
-                        <p key={i}>
-                            index = {i}: {x}
-                        </p>
-                    )
-                })}
+  function handleSlice (e) {
+    e.preventDefault();
+  }
+
+  return (
+    <>
+      <div className="cards">
+        <div className="card">
+          <h3>.slice()</h3>
+          <p>
+            it can take 2 arguments: the first is the index where the slice
+            should begin and the second is the index before which it should end
+          </p>
+          <div className="horizontal">
+            <form onSubmit={handleSlice}>
+              <button>Slice</button>
+              <input
+                type="text"
+                id="first"
+                value={first}
+                onChange={(e) => setFirst(e.target.value)}
+              ></input>
+              <input
+                type="text"
+                id="second"
+                value={second}
+                onChange={(e) => setSecond(e.target.value)}
+              ></input>
+            </form>
             </div>
+          <div>
+            {array.map((x, i) => {
+              return (
+                <p key={i}>
+                  index = {i}: {x}
+                </p>
+              );
+            })}
           </div>
         </div>
-      </>
-    );
-
+      </div>
+    </>
+  );
 }
 
 export default Slice;
