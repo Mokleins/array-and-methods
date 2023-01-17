f776f7be6ca45e3b06f019137fedaf4b6b7e5c0c
Xylon Funes <xyfullns@gmail.com>
2023-01-16 23:56:32 -0500

    commit

--- src/components/Slice.js
+++ src/components/Slice.js
@@ -42,7 +42,7 @@ function Slice() {
           </p>
           <div className="horizontal">
             <form onSubmit={handleSlice}>
-              <button>Slice</button>
+              <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Slice</button>
               <input
                 type="text"
                 id="first"
