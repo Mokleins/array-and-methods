f8c2c349f3278849215dcb7b8cb2c04bce3d3f33
Xylon Funes <xyfullns@gmail.com>
2023-01-16 23:31:38 -0500

    tail

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "src/App.js",
+    "src/components/Push.js",
+    "src/components/Slice.js",
+    "src/components/Unshift.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
